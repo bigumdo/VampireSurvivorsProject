2216116800c38e1c41ee95db1de0c56c27e52b58
bigumdo <showmethe0610@gmail.com>
2024-12-26 20:42:53 +0900

    CustomEditorTest

--- Assets/01.Scripts/Editor/TestEditor.cs
+++ Assets/01.Scripts/Editor/TestEditor.cs
@@ -1,39 +1,69 @@
 #if UNITY_EDITOR
+using System;
+using Unity.VisualScripting;
 using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 namespace BGD.Editor
 {
     public class TestEditor : EditorWindow
     {
-        private GameObject gameObjectVar;
-
+        private bool TestToggle;
         private int intVar;
-        private float floatVar;
-        private Vector2 vector2Var;
-        private Vector3 vector3Var;
-        private Color colorVar;
+        private Number TestEnum;
+        private bool boolVar;
+        public enum Number
+        {
+            One,Two, Three
+        }
+
 
         [MenuItem("Custom/EditorWindow")] //해당 버튼을 누르면 Init 함수가 실행
         private static void Init()
         {
-            TestEditor editorWindow = (TestEditor)GetWindow(typeof(TestEditor)); //Window 생성
+            //Window 생성()안에 string으로 글자를 넣으면 창에 글자를 원하는 대로 바꿀 수 있다.
+            //()에 string을 넣지 않으면 클래스 이름이 뜬다.
+            TestEditor editorWindow = GetWindow<TestEditor>("BGDTestEditor");
+            editorWindow.minSize = new Vector2(700, 500);
             editorWindow.Show(); //Window 열기
         }
 
+        
+
         private void OnGUI()
         {
-            EditorGUILayout.LabelField("My Label");
-            EditorGUILayout.LabelField("My BoldLabel", EditorStyles.boldLabel);
+            intVar = GUILayout.Toolbar(intVar, System.Enum.GetNames(typeof(Number)));
 
-            gameObjectVar = (GameObject)EditorGUILayout.ObjectField("My GameObject"
-                , gameObjectVar, typeof(GameObject), true);
+            switch (intVar)
+            {
+                case 0:
+                    EditorGUILayout.LabelField("Test1");
+                    TestToggle = EditorGUILayout.Toggle("ToggleTest", TestToggle);
+                    boolVar = EditorGUILayout.Foldout(boolVar, "My FoldOut", true);//글자를 클릭해도 토글될 것인가
+                    if(boolVar)
+                    {
+                        ++EditorGUI.indentLevel;
+                        GUILayout.BeginVertical();
+                        GUILayout.Label("Test1");
+                        GUILayout.Label("Test2");
+                        GUILayout.Label("Test3");
+                        GUILayout.EndVertical();
+                        --EditorGUI.indentLevel;
+                    }
+                    EditorGUILayout.LabelField("Toggle");
+                        
+                    break;
+                case 1:
+                    EditorGUILayout.LabelField("Test2");
+                    GUILayout.Button("ButtonTest");
+                    break;
+                case 2:
+                    EditorGUILayout.LabelField("Test3");
+                    TestEnum = (Number)EditorGUILayout.EnumPopup("My Enum", TestEnum);
+                    break;
+            }
 
-            intVar = EditorGUILayout.IntField("My Int", intVar);
-            floatVar = EditorGUILayout.FloatField("My Float", floatVar);
-            vector2Var = EditorGUILayout.Vector2Field("My Vector2", vector2Var);
-            vector3Var = EditorGUILayout.Vector3Field("My Vector3", vector3Var);
-            colorVar = EditorGUILayout.ColorField("My Color", colorVar);
         }
     }
 }
